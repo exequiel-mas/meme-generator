80a738ebc94ed26cca6023c4c90c61c2c824f45c
exequiel-mas <exe.massimelli@gmail.com>
2022-07-24 14:21:06 -0300

    bug fixed

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -23,9 +23,6 @@ export default function Meme() {
   //     }
   // }, [])
 
-  const [memeImage, setMemeImage] = React.useState(
-    'http://i.imgflip.com/1bij.jpg'
-  );
   const [meme, setMeme] = React.useState({
     topText: '',
     bottomText: '',
@@ -72,7 +69,7 @@ export default function Meme() {
         </button>
       </div>
       <div className="meme">
-        <img src={meme.randomImage} className="meme--image" />
+        <img src={meme.randomImage} className="meme--image" alt={meme.name} />
         <h2 className="meme--text top">{meme.topText}</h2>
         <h2 className="meme--text bottom">{meme.bottomText}</h2>
       </div>
