4a8d75e20d672a434186277a99df7fa1ea12f0d4
exequiel-mas <exe.massimelli@gmail.com>
2022-07-24 14:25:24 -0300

    deploy bug fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import logoMeme from '../images/troll-face.png';
 const Header = () => {
   return (
     <header className="header">
-      <img src={logoMeme} className="header--image" />
+      <img src={logoMeme} className="header--image" alt="" />
       <h2 className="header--title">Meme Generator</h2>
       <h4 className="header--project">React Course - Project 3</h4>
     </header>
